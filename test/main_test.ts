fff9fef1b4dc66a319179e0b399615a286ff335f
James Garbutt <43081j@users.noreply.github.com>
2025-09-02 14:08:21 +0100

    initial fluff

---
+++ test/main_test.ts
@@ -0,0 +1,7 @@
+import {describe, it, expect} from "vitest";
+
+describe("huh", () => {
+    it("works", () => {
+        expect(true).toBe(true);
+    });
+});
---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import {defineConfig} from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: [
+      "test/**/*_test.ts",
+    ],
+    coverage: {
+      reporter: ['text', 'lcov'],
+      include: [
+        "src/**/*.ts"
+      ]
+    }
+  }
+});
